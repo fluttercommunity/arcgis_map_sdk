a3f9167979cf8e88cfd68a088720177afad0f0ab
Julian Bissekkou <julian.bissekkou@tapped.dev>
2022-10-10 17:23:54 +0200

    create empty MethodChannelArcgisMapPlugin

--- arcgis_map/lib/src/method_channel_arcgis_map_plugin.dart
+++ arcgis_map/lib/src/method_channel_arcgis_map_plugin.dart
@@ -1,27 +1,17 @@
 import 'package:arcgis_map_platform_interface/arcgis_map_platform_interface.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-class ArcgisMapPlatform extends PlatformInterface {
-  ArcgisMapPlatform() : super(token: _token);
+const _methodChannel = MethodChannel("esri.arcgis.flutter_plugin");
 
-  static late ArcgisMapPlatform _instance;
-
-  static final Object _token = Object();
-
-  static ArcgisMapPlatform get instance => _instance;
-
-  static set instance(ArcgisMapPlatform instance) {
-    PlatformInterface.verifyToken(instance, _token);
-    _instance = instance;
-  }
-
+class MethodChannelArcgisMapPlugin implements ArcgisMapPlatform {
   /// This method is called when the plugin is first initialized.
+  @override
   Future<void> init(int mapId) {
     throw UnimplementedError('init() has not been implemented.');
   }
 
+  @override
   Future<FeatureLayer> addFeatureLayer(
     FeatureLayerOptions options,
     List<Graphic>? data,
@@ -34,46 +24,57 @@ class ArcgisMapPlatform extends PlatformInterface {
     throw UnimplementedError('addFeatureLayer() has not been implemented.');
   }
 
+  @override
   Stream<double> getZoom(int mapId) {
     throw UnimplementedError('getZoom() has not been implemented');
   }
 
+  @override
   void setMouseCursor(SystemMouseCursor cursor, int mapId) {
     throw UnimplementedError('setMouseCursor() has not been implemented');
   }
 
+  @override
   void updateGraphicSymbol(Symbol symbol, String polygonId, int mapId) {
     throw UnimplementedError('updateGraphicSymbol() has not been implemented');
   }
 
+  @override
   Stream<LatLng> centerPosition(int mapId) {
     throw UnimplementedError('centerPosition() has not been implemented');
   }
 
+  @override
   Stream<List<String>> visibleGraphics(int mapId) {
     throw UnimplementedError('visibleGraphics() has not been implemented.');
   }
 
+  @override
   Stream<BoundingBox> getBounds(int mapId) {
     throw UnimplementedError('getBounds() has not been implemented.');
   }
 
+  @override
   Stream<String> attributionText(int mapId) {
     throw UnimplementedError('attributionText() has not been implemented.');
   }
 
+  @override
   void onClick(void Function(ArcGisMapAttributes?) onPressed, int mapId) {
     throw UnimplementedError('onClick() has not been implemented.');
   }
 
+  @override
   Future<void> updateFeatureLayer(List<Graphic> data, int mapId) async {
     throw UnimplementedError('addFeatureLayer() has not been implemented.');
   }
 
+  @override
   bool destroyFeatureLayer({required String layerId, required int mapId}) {
     throw UnimplementedError('destroyFeatureLayer() has not been implemented.');
   }
 
+  @override
   bool graphicContainsPoint(
       {required String polygonId,
       required LatLng pointCoordinates,
@@ -82,10 +83,12 @@ class ArcgisMapPlatform extends PlatformInterface {
         'graphicContainsPoint() has not been implemented.');
   }
 
+  @override
   void dispose({required int mapId}) {
     throw UnimplementedError('dispose() has not been implemented.');
   }
 
+  @override
   Widget buildView({
     required int creationId,
     required PlatformViewCreatedCallback onPlatformViewCreated,
@@ -94,6 +97,7 @@ class ArcgisMapPlatform extends PlatformInterface {
     throw UnimplementedError('buildView() has not been implemented.');
   }
 
+  @override
   Future<void> moveCamera({
     required LatLng point,
     required int mapId,
@@ -103,34 +107,42 @@ class ArcgisMapPlatform extends PlatformInterface {
     throw UnimplementedError('moveCamera() has not been implemented.');
   }
 
+  @override
   Future<void> zoomIn(int lodFactor, int mapId) {
     throw UnimplementedError('zoomIn() has not been implemented.');
   }
 
+  @override
   Future<void> zoomOut(int lodFactor, int mapId) {
     throw UnimplementedError('zoomOut() has not been implemented.');
   }
 
+  @override
   void addGraphic(int mapId, Graphic graphic) {
     throw UnimplementedError('addGraphic() has not been implemented.');
   }
 
+  @override
   void removeGraphic(int mapId, String graphicId) {
     throw UnimplementedError('removeGraphic() has not been implemented.');
   }
 
+  @override
   void addViewPadding(int mapId, ViewPadding padding) {
     throw UnimplementedError('addViewPadding() has not been implemented.');
   }
 
+  @override
   void toggleBaseMap(int mapId, BaseMap baseMap) {
     throw UnimplementedError('toggleBaseMap() has not been implemented.');
   }
 
+  @override
   List<Graphic> getGraphicsInView(int mapId) {
     throw UnimplementedError('getGraphicsInView() has not been implemented.');
   }
 
+  @override
   List<String> getVisibleGraphicIds(int mapId) {
     throw UnimplementedError(
         'getVisibleGraphicIds() has not been implemented.');
