7f7c69a59099f44ff1cb938688b6613e336696bf
Julian Bissekkou <julian.bissekkou@tapped.dev>
2022-10-12 07:30:31 +0200

    smaller improvements on android

--- arcgis_map/android/src/main/kotlin/esri/arcgis/flutter_plugin/parsing.kt
+++ arcgis_map/android/src/main/kotlin/esri/arcgis/flutter_plugin/parsing.kt
@@ -36,18 +36,17 @@ class BasemapStyleAdapter : TypeAdapter<BasemapStyle>() {
     }
 }
 
-fun BasemapStyle.getJsonValue(): String {
-
+fun BasemapStyle.getJsonValue(): String? {
     return when (this) {
         BasemapStyle.ARCGIS_IMAGERY -> "arcgisImagery"
         BasemapStyle.ARCGIS_IMAGERY_STANDARD -> "arcgisImageryStandard"
         BasemapStyle.ARCGIS_IMAGERY_LABELS -> "arcgisImageryLabels"
         BasemapStyle.ARCGIS_LIGHT_GRAY -> "arcgisLightGray"
-        BasemapStyle.ARCGIS_LIGHT_GRAY_BASE -> TODO()
-        BasemapStyle.ARCGIS_LIGHT_GRAY_LABELS -> TODO()
+        BasemapStyle.ARCGIS_LIGHT_GRAY_BASE -> null
+        BasemapStyle.ARCGIS_LIGHT_GRAY_LABELS -> null
         BasemapStyle.ARCGIS_DARK_GRAY -> "arcgisDarkGray"
-        BasemapStyle.ARCGIS_DARK_GRAY_BASE -> TODO()
-        BasemapStyle.ARCGIS_DARK_GRAY_LABELS -> TODO()
+        BasemapStyle.ARCGIS_DARK_GRAY_BASE -> null
+        BasemapStyle.ARCGIS_DARK_GRAY_LABELS -> null
         BasemapStyle.ARCGIS_NAVIGATION -> "arcgisNavigation"
         BasemapStyle.ARCGIS_NAVIGATION_NIGHT -> "arcgisNavigationNight"
         BasemapStyle.ARCGIS_STREETS -> "arcgisStreets"
@@ -55,11 +54,11 @@ fun BasemapStyle.getJsonValue(): String {
         BasemapStyle.ARCGIS_STREETS_RELIEF -> "arcgisStreetsRelief"
         BasemapStyle.ARCGIS_TOPOGRAPHIC -> "arcgisTopographic"
         BasemapStyle.ARCGIS_OCEANS -> "arcgisOceans"
-        BasemapStyle.ARCGIS_OCEANS_BASE -> TODO()
-        BasemapStyle.ARCGIS_OCEANS_LABELS -> TODO()
+        BasemapStyle.ARCGIS_OCEANS_BASE -> null
+        BasemapStyle.ARCGIS_OCEANS_LABELS -> null
         BasemapStyle.ARCGIS_TERRAIN -> "arcgisTerrain"
-        BasemapStyle.ARCGIS_TERRAIN_BASE -> TODO()
-        BasemapStyle.ARCGIS_TERRAIN_DETAIL -> TODO()
+        BasemapStyle.ARCGIS_TERRAIN_BASE -> null
+        BasemapStyle.ARCGIS_TERRAIN_DETAIL -> null
         BasemapStyle.ARCGIS_COMMUNITY -> "arcgisCommunity"
         BasemapStyle.ARCGIS_CHARTED_TERRITORY -> "arcgisChartedTerritory"
         BasemapStyle.ARCGIS_COLORED_PENCIL -> "arcgisColoredPencil"
@@ -69,21 +68,21 @@ fun BasemapStyle.getJsonValue(): String {
         BasemapStyle.ARCGIS_NEWSPAPER -> "arcgisNewspaper"
         BasemapStyle.ARCGIS_HILLSHADE_LIGHT -> "arcgisHillshadeLight"
         BasemapStyle.ARCGIS_HILLSHADE_DARK -> "arcgisHillshadeDark"
-        BasemapStyle.ARCGIS_STREETS_RELIEF_BASE -> TODO()
-        BasemapStyle.ARCGIS_TOPOGRAPHIC_BASE -> TODO()
-        BasemapStyle.ARCGIS_CHARTED_TERRITORY_BASE -> TODO()
-        BasemapStyle.ARCGIS_MODERN_ANTIQUE_BASE -> TODO()
+        BasemapStyle.ARCGIS_STREETS_RELIEF_BASE -> null
+        BasemapStyle.ARCGIS_TOPOGRAPHIC_BASE -> null
+        BasemapStyle.ARCGIS_CHARTED_TERRITORY_BASE -> null
+        BasemapStyle.ARCGIS_MODERN_ANTIQUE_BASE -> null
         BasemapStyle.OSM_STANDARD -> "osmStandard"
         BasemapStyle.OSM_STANDARD_RELIEF -> "osmStandardRelief"
-        BasemapStyle.OSM_STANDARD_RELIEF_BASE -> TODO()
+        BasemapStyle.OSM_STANDARD_RELIEF_BASE -> null
         BasemapStyle.OSM_STREETS -> "osmStreets"
         BasemapStyle.OSM_STREETS_RELIEF -> "osmStreetsRelief"
         BasemapStyle.OSM_LIGHT_GRAY -> "osmLightGray"
-        BasemapStyle.OSM_LIGHT_GRAY_BASE -> TODO()
-        BasemapStyle.OSM_LIGHT_GRAY_LABELS -> TODO()
+        BasemapStyle.OSM_LIGHT_GRAY_BASE -> null
+        BasemapStyle.OSM_LIGHT_GRAY_LABELS -> null
         BasemapStyle.OSM_DARK_GRAY -> "osmDarkGray"
-        BasemapStyle.OSM_DARK_GRAY_BASE -> TODO()
-        BasemapStyle.OSM_DARK_GRAY_LABELS -> TODO()
-        BasemapStyle.OSM_STREETS_RELIEF_BASE -> TODO()
+        BasemapStyle.OSM_DARK_GRAY_BASE -> null
+        BasemapStyle.OSM_DARK_GRAY_LABELS -> null
+        BasemapStyle.OSM_STREETS_RELIEF_BASE -> null
     }
 }
