ff917fd9f0a337beb64806fbb635225ad3d99652
Stefan Schaller <stefan.schaller@tapped.dev>
2022-12-09 15:44:00 +0100

    remove not correct values

--- arcgis_map_android/android/src/main/kotlin/esri/arcgis/flutter_plugin/model/symbol/SimpleLineSymbolPayload.kt
+++ arcgis_map_android/android/src/main/kotlin/esri/arcgis/flutter_plugin/model/symbol/SimpleLineSymbolPayload.kt
@@ -2,14 +2,11 @@ package esri.arcgis.flutter_plugin.model.symbol
 
 import esri.arcgis.flutter_plugin.model.MapColor
 
-//TODO add marker
+//TODO 
 data class SimpleLineSymbolPayload(
-    val cap: String,
     val color: MapColor?,
     val declaredClass: String?,
-    val join: String,
     val miterLimit: Double?,
-    val style: String,
     val width: Double,
 )
 
@@ -17,3 +14,4 @@ data class SimpleLineSymbolPayload(
 data class LineSymbolMarker(
     val color: MapColor
 )
+
