b0bb793ab38b93cebf1e90fd4e65454ffbf635c1
MatthaiosSait <matthaios.saitakis@gmail.com>
2023-11-13 17:10:41 +0100

    Add `const` to LatLng

--- arcgis_map/example/lib/vector_layer_example_page.dart
+++ arcgis_map/example/lib/vector_layer_example_page.dart
@@ -19,7 +19,7 @@ class _VectorLayerExamplePageState extends State<VectorLayerExamplePage> {
         children: [
           ArcgisMap(
             apiKey: arcGisApiKey,
-            initialCenter: LatLng(51.16, 10.45),
+            initialCenter: const LatLng(51.16, 10.45),
             zoom: 13,
             vectorTileLayerUrls: const [
               "https://basemaps.arcgis.com/arcgis/rest/services/World_Basemap_v2/VectorTileServer",
