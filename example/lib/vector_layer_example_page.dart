70a92a4e2d93e4daf481dc2b8b1b3c1b2155cce1
Julian Bissekkou <julian.bissekkou@tapped.dev>
2024-02-05 08:32:10 +0100

    add example for reloading

--- example/lib/vector_layer_example_page.dart
+++ example/lib/vector_layer_example_page.dart
@@ -10,22 +10,27 @@ class VectorLayerExamplePage extends StatefulWidget {
 }
 
 class _VectorLayerExamplePageState extends State<VectorLayerExamplePage> {
+  ArcgisMapController? _controller;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Stack(
-        children: [
-          ArcgisMap(
-            apiKey: arcGisApiKey,
-            initialCenter: const LatLng(51.16, 10.45),
-            zoom: 13,
-            vectorTileLayerUrls: const [
-              "https://basemaps.arcgis.com/arcgis/rest/services/World_Basemap_v2/VectorTileServer",
-            ],
-            mapStyle: MapStyle.twoD,
-          ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.refresh),
+        onPressed: () => _controller?.reload(),
+      ),
+      body: ArcgisMap(
+        apiKey: arcGisApiKey,
+        onMapCreated: (controller) {
+          _controller = controller;
+        },
+        initialCenter: const LatLng(51.16, 10.45),
+        zoom: 13,
+        vectorTileLayerUrls: const [
+          "https://basemaps.arcgis.com/arcgis/rest/services/World_Basemap_v2/VectorTileServer",
         ],
+        mapStyle: MapStyle.twoD,
       ),
     );
   }
