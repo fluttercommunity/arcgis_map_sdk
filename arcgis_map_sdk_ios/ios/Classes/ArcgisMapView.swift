5e87aa45d87cf7f5ecc7db5db4497c76fd976baa
Julian Bissekkou <julian.bissekkou@tapped.dev>
2024-02-27 14:02:23 +0100

    fix ios build

--- arcgis_map_sdk_ios/ios/Classes/ArcgisMapView.swift
+++ arcgis_map_sdk_ios/ios/Classes/ArcgisMapView.swift
@@ -12,8 +12,6 @@ class ArcgisMapView: NSObject, FlutterPlatformView {
     private let centerPositionStreamHandler = CenterPositionStreamHandler()
     private let flutterPluginRegistrar: FlutterPluginRegistrar
 
-    private let flutterPluginRegistrar: FlutterPluginRegistrar
-
     private var mapLoadStatusObservation: NSKeyValueObservation?
 
     private var mapScaleObservation: NSKeyValueObservation?
@@ -119,7 +117,7 @@ class ArcgisMapView: NSObject, FlutterPlatformView {
         let viewpoint = AGSViewpoint(
                 latitude: mapOptions.initialCenter.latitude,
                 longitude: mapOptions.initialCenter.longitude,
-                scale: getMapScale(Int(mapOptions.zoom))
+                scale: convertZoomLevelToMapScale(Int(mapOptions.zoom))
         )
         mapView.setViewpoint(viewpoint)
 
@@ -640,9 +638,4 @@ extension AGSLoadStatus {
             return "unknown"
         }
     }
-}
-
-struct MoveToPointsPayload : Codable {
-    let points : [LatLng]
-    let padding : Double?
 }
