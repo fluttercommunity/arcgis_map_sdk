d6c91a18d71c08a3df240f050551059bebf046eb
sbergmair <github@sbergmair.de>
2024-08-19 19:30:17 +0200

    refactor code a bit

--- arcgis_map_sdk/lib/src/arcgis_location_display.dart
+++ arcgis_map_sdk/lib/src/arcgis_location_display.dart
@@ -1,4 +1,4 @@
-import 'package:arcgis_map_sdk/src/model/auto_pan_mode.dart';
+import 'package:arcgis_map_sdk_platform_interface/src/types/auto_pan_mode.dart';
 import 'package:arcgis_map_sdk_platform_interface/arcgis_map_sdk_platform_interface.dart';
 
 /// The use case for manual location displays is relevant when the application
