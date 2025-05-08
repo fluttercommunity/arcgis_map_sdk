53b1e86dbb85f6c42cbc08384dbe1c2390adc984
Tarek Tolba <tarek.tolba@tapped.dev>
2025-05-08 17:42:15 +0300

    Upgrade android SDK to 200.7

--- arcgis_map_sdk_android/android/src/main/kotlin/dev/fluttercommunity/arcgis_map_sdk_android/ArcgisMapViewFactory.kt
+++ arcgis_map_sdk_android/android/src/main/kotlin/dev/fluttercommunity/arcgis_map_sdk_android/ArcgisMapViewFactory.kt
@@ -1,19 +1,33 @@
 package dev.fluttercommunity.arcgis_map_sdk_android
 
 import android.content.Context
+import android.content.ContextWrapper
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
 import dev.fluttercommunity.arcgis_map_sdk_android.model.ArcgisMapOptions
 import io.flutter.embedding.engine.plugins.FlutterPlugin.FlutterPluginBinding
 import io.flutter.plugin.common.StandardMessageCodec
 import io.flutter.plugin.platform.PlatformView
 import io.flutter.plugin.platform.PlatformViewFactory
-import dev.fluttercommunity.arcgis_map_sdk_android.ArcgisMapView
 
-class ArcgisMapViewFactory(private val flutterPluginBinding: FlutterPluginBinding) :
-    PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+class ArcgisMapViewFactory(
+    private val flutterPluginBinding: FlutterPluginBinding,
+) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
     override fun create(context: Context?, viewId: Int, args: Any?): PlatformView {
         val optionParams = args as Map<String, Any>
         val params = optionParams.parseToClass<ArcgisMapOptions>()
 
-        return ArcgisMapView(context!!, viewId, params, flutterPluginBinding)
+        return ArcgisMapView(
+            context!!, viewId, params, flutterPluginBinding, getLifecycle(context)!!
+        )
+    }
+
+    private fun getLifecycle(context: Context): Lifecycle? {
+        var currentContext = context
+        while (currentContext is ContextWrapper) {
+            if (currentContext is LifecycleOwner) return currentContext.lifecycle
+            currentContext = currentContext.baseContext
+        }
+        return null
     }
 }
