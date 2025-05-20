ba89d0f83b943a2c865352a7a210a91736e736b4
sbergmair <github@sbergmair.de>
2025-05-20 10:41:13 +0200

    fix map status callback

--- arcgis_map_sdk_ios/ios/arcgis_map_sdk_ios/Sources/arcgis_map_sdk_ios/MapContentView.swift
+++ arcgis_map_sdk_ios/ios/arcgis_map_sdk_ios/Sources/arcgis_map_sdk_ios/MapContentView.swift
@@ -34,11 +34,6 @@ struct MapContentView: View {
             .onVisibleAreaChanged { polygon in
                 viewModel.onVisibleAreaChanged?(polygon)
             }
-            .onChange(of: viewModel.map.basemap?.loadStatus) { newValue in
-                if let newValue {
-                    viewModel.onLoadStatusChanged?(newValue)
-                }
-            }
             .task {
                 // Store the mapViewProxy for external access
                 viewModel.mapViewProxy = mapViewProxy
@@ -50,6 +45,12 @@ struct MapContentView: View {
                 viewModel.mapViewProxy = nil
             }
             .ignoresSafeArea(edges: .all)
+            .task {
+                // Listen for load status changes and set the load status text.
+                for await loadStatus in viewModel.map.$loadStatus {
+                    viewModel.onLoadStatusChanged?(loadStatus)
+                }
+            }
         }
     }
 }
